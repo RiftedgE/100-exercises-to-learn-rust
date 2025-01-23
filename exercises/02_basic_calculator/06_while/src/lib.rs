28047c0b51745005b367603b071f802f56eb59a5
Cloud.Liu <cloud.liu@fanruan.com>
2025-01-23 14:13:04 +0800

    exercises

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,19 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n == 0 {
+        return 1;
+    } else if n == 1 {
+        return 1;
+    }
+
+    let mut nn = n;
+    let mut result: u32 = 1;
+    while nn > 0 {
+        result *= nn;
+        nn -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
